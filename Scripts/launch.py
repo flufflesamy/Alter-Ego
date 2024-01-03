9485d63a1240f990011a85a988da9f88cd32ba22
Amy Poon <amy@fluffles.me>
2024-01-03 00:16:10 -0500

    add launch.py

--- Scripts/launch.py
+++ Scripts/launch.py
@@ -1,3 +1,12 @@
+import subprocess
+import sys
 import write_config
 
 write_config.write()
+
+with open('../alter-ego.log', 'a') as logfile:
+    proc = subprocess.Popen(['node', '../app.js'], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for line in proc.stdout:
+        sys.stdout.write(line)
+        logfile.write(line)
+    proc.wait()
